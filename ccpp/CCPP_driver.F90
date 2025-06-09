dc7b73e67586ea9fb06952a15d102a7c40b0aeb9
Jun.Wang <Jun.Wang@noaa.gov>
2025-06-09 14:59:59 +0000

    fix zero out accumulated total precip

--- ccpp/CCPP_driver.F90
+++ ccpp/CCPP_driver.F90
@@ -59,6 +59,8 @@ module CCPP_driver
     ! Local variables
     integer :: nb, nt, ntX
     integer :: ierr2
+    integer :: kdt_iau
+    logical :: iauwindow_center
     ! DH* 20210104 - remove kdt_rad when code to clear diagnostic buckets is removed
     integer :: kdt_rad
 
@@ -167,8 +169,16 @@ module CCPP_driver
       endif
 
       !--- determine if physics diagnostics buckets need to be cleared
-      if ((mod(GFS_control%kdt-1,GFS_control%nszero)) == 0) then
-        call GFS_Intdiag%phys_zero(GFS_control)
+      if (GFS_control%iau_offset > 0) then
+        kdt_iau = nint(GFS_control%iau_offset*3600./GFS_control%dtp)
+        if (GFS_control%kdt-1 == kdt_iau) then
+          iauwindow_center = .true.
+        else
+          iauwindow_center = .false.
+        endif
+      endif
+      if ((mod(GFS_control%kdt-1,GFS_control%nszero)) == 0 .or. iauwindow_center) then
+        call GFS_Intdiag%phys_zero(GFS_control, iauwindow_center=iauwindow_center)
       endif
 
       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
